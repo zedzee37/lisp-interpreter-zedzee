2124adee50a274899ef6cc8da69287ae77090210
zedzee <carsonuderitz@gmail.com>
2025-04-11 16:28:34 -0400

    removed some stuff

--- src/parser.c
+++ src/parser.c
@@ -10,45 +10,5 @@ Expr **parse(const char *source, size_t *tokensSize) {
 	*tokensSize = 0;
 	Expr **exprs = calloc(currentSize, sizeof(Expr*));
 
-	// Example
-	// size_t current = 0;
-	// size_t exprSize;
-	// Expr *expr = parseExpr(source + current, &exprSize);
-	// current += exprSize;
-
-	size_t current = 0;
-	size_t sourceLen = strlen(source);
-
-	while (current < sourceLen) {
-		char ch = source[current];
-
-		switch (ch) {
-			case '(':
-			break;
-
-			case ' ':
-			break;
-			case '\n':
-			break;
-			case '\t':
-			break;
-			case '\r':
-			break;
-
-			default:
-			// An error occurred, all expressions must start with parenthesis
-			// TODO: better error reporting
-			printf("Error\n");
-			return NULL;
-			break;
-		}
-
-		current++;
-	}
-
 	return exprs;
-}
-
-Expr *parseExpr(const char *source, size_t *exprLen) {
-	(*exprLen) = 1; // 1 to consume the parenthesis
 }
