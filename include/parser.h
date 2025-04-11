59e19b6bc97fd0348bd3b302b88a40440d0b2bf5
zedzee <carsonuderitz@gmail.com>
2025-04-11 16:45:02 -0400

    better fucntion structure

--- include/parser.h
+++ include/parser.h
@@ -3,4 +3,14 @@
 #include <stddef.h>
 #include <expr.h>
 
+typedef struct {
+	const char *source;
+	size_t sourceLength;
+	size_t current;
+	size_t line;
+	Expr **exprs;
+	size_t exprsSize;
+	size_t exprsCount;
+} Parser;
+
 Expr **parse(const char *source, size_t *exprsSize);
--- src/parser.c
+++ src/parser.c
@@ -5,10 +5,54 @@
 #include <string.h>
 #include <stdio.h>
 
+// parse parses the entire string into expressions, whilst parseExpr parses a single expression
+
+void consumeWhitespace(Parser *parser) {
+	while (parser->current < parser->sourceLength) {
+		char ch = parser->source[parser->current];
+
+		switch (ch) {
+			case ' ':
+			break;
+			case '\t':
+			break;
+			case '\r':
+			break;
+			case '\n':
+			parser->line++;
+			break;
+			default:
+			return;
+		}
+
+		parser->current++;
+	}
+}
+
+Expr *parseExpr(Parser *parser) {
+	consumeWhitespace(parser);
+	char ch = parser->source[parser->current];
+
+	if (ch != '(') {
+		// TODO: error here
+		return NULL;
+	}
+}
+
 Expr **parse(const char *source, size_t *tokensSize) {
 	size_t currentSize = 10;
-	*tokensSize = 0;
-	Expr **exprs = calloc(currentSize, sizeof(Expr*));
+	Expr **exprs = calloc(currentSize, sizeof(Expr *));
 
+	Parser parser = {
+		.source = source,
+		.sourceLength = strlen(source),
+		.current = 0,
+		.line = 1,
+		.exprs = exprs,
+		.exprsSize = currentSize,
+		.exprsCount = 0
+	};
+
+	*tokensSize = parser.exprsCount;
 	return exprs;
 }
