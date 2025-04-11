b0bd8b7018a6827b43942f35f24e17d7758952f7
zedzee <carsonuderitz@gmail.com>
2025-04-11 12:43:33 -0400

    split expr.h and parser.h

---
+++ include/expr.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef enum {
+	LITERAL,
+	GROUPING,
+	LIST,
+	IDENTIFIER
+} ExprType;
+
+typedef struct {
+	ExprType type;
+} Expr;
+
+typedef struct {
+	Expr base;
+	union {
+		char *string;
+		bool boolean;
+		double number;
+	} data;
+} LiteralExpr;
+
+typedef struct {
+	Expr base;
+	Expr *expr;
+} GroupingExpr;
+
+typedef struct {
+	Expr base;
+	Expr *left;
+	Expr **right;
+	size_t rightSize;
+} ListExpr;
+
+typedef struct {
+	Expr base;
+	char *name;
+} IdentifierExpr;
--- include/parser.h
+++ include/parser.h
@@ -1,35 +1,6 @@
 #pragma once
 
-#include <stdbool.h>
 #include <stddef.h>
+#include <expr.h>
 
-typedef enum {
-	LITERAL,
-	GROUPING,
-	LIST
-} ExprType;
-
-typedef struct {
-	ExprType type;
-} Expr;
-
-typedef struct {
-	Expr base;
-	union {
-		char *string;
-		bool boolean;
-		double number;
-	} data;
-} LiteralExpr;
-
-typedef struct {
-	Expr base;
-	Expr *expr;
-} GroupingExpr;
-
-typedef struct {
-	Expr base;
-	Expr *left;
-	Expr **right;
-	size_t rightSize;
-} ListExpr;
+Expr *parse(const char *source, size_t *tokensSize);
