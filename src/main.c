feace442e0402bf73f9d1372342b24d936a113f1
zedzee <carsonuderitz@gmail.com>
2025-04-14 20:48:23 -0400

    fix tabs

--- src/main.c
+++ src/main.c
@@ -6,117 +6,117 @@
 #include <stdbool.h>
 
 char *readFile(char *fileName) {
-	FILE *fp = fopen(fileName, "r");
-	if (!fp) {
-		return NULL;
-	}
+    FILE *fp = fopen(fileName, "r");
+    if (!fp) {
+        return NULL;
+    }
 
-	fseek(fp, 0, SEEK_END);
-	size_t size = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
+    fseek(fp, 0, SEEK_END);
+    size_t size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
 
-	char *fileContents = calloc(size + 1, sizeof(char));
-	if (!fileContents) {
-		return NULL;
-	}
+    char *fileContents = calloc(size + 1, sizeof(char));
+    if (!fileContents) {
+        return NULL;
+    }
 
-	fileContents[size] = '\0';
+    fileContents[size] = '\0';
 
-	fread(fileContents, sizeof(char), size, fp);
+    fread(fileContents, sizeof(char), size, fp);
 
-	fclose(fp);
-	return fileContents;
+    fclose(fp);
+    return fileContents;
 }
 
 void reportError(ParserError err) {
-	switch (err.errorType) {
-		case UNEXPECTED_CHAR:
-		printf("Unexpected character at %lu, found %c.\n", err.line, err.ch);
-		break;
-		case MISSING_CHAR:
-		printf("Missing character at %lu, expected %c.\n", err.line, err.ch);
-		break;
-		default:
-		break;
-	}
-	return;
+    switch (err.errorType) {
+        case UNEXPECTED_CHAR:
+        printf("Unexpected character at %lu, found %c.\n", err.line, err.ch);
+        break;
+        case MISSING_CHAR:
+        printf("Missing character at %lu, expected %c.\n", err.line, err.ch);
+        break;
+        default:
+        break;
+    }
+    return;
 }
 
 void run(char *source) {
-	size_t exprCount;
-	Expr **exprs;
-	ParserError err = parse(source, &exprCount, &exprs);
+    size_t exprCount;
+    Expr **exprs;
+    ParserError err = parse(source, &exprCount, &exprs);
 
-	if (err.errorType != NONE) {
-		reportError(err);
-		return;
-	}
+    if (err.errorType != NONE) {
+        reportError(err);
+        return;
+    }
 
-	for (int i = 0; i < exprCount; i++) {
-		Expr *expr = exprs[i];
+    for (int i = 0; i < exprCount; i++) {
+        Expr *expr = exprs[i];
 
-		LiteralExpr *literal = (LiteralExpr *)expr;
-		switch (expr->type) {
-			case LITERAL:
-			switch (literal->type) {
-				case STRING:
-				printf("Literal %s\n", literal->string);
-				break;
-				case NUMBER:
-				printf("Literal %f\n", literal->number);
-				break;
-				case BOOLEAN:
-				break;
-			}
-		 	break;
-			case LIST:
-			printf("List %lu\n", ((ListExpr *)expr)->exprsCount);
-			break;
-			case IDENTIFIER:
-			printf("Identifier '%s'\n", ((IdentifierExpr *)expr)->name);
-			break;
-		}
+        LiteralExpr *literal = (LiteralExpr *)expr;
+        switch (expr->type) {
+            case LITERAL:
+            switch (literal->type) {
+                case STRING:
+                printf("Literal %s\n", literal->string);
+                break;
+                case NUMBER:
+                printf("Literal %f\n", literal->number);
+                break;
+                case BOOLEAN:
+                break;
+            }
+            break;
+            case LIST:
+            printf("List %lu\n", ((ListExpr *)expr)->exprsCount);
+            break;
+            case IDENTIFIER:
+            printf("Identifier '%s'\n", ((IdentifierExpr *)expr)->name);
+            break;
+        }
 
-		freeExpr(expr);
-	}
-	free(exprs);
+        freeExpr(expr);
+    }
+    free(exprs);
 }
 
 void runFile(char *fileName) {
-	char *fileContents = readFile(fileName);
-	if (!fileContents) {
-		return;
-	}
+    char *fileContents = readFile(fileName);
+    if (!fileContents) {
+        return;
+    }
 
-	run(fileContents);
-	free(fileContents);
+    run(fileContents);
+    free(fileContents);
 }
 
 void runPrompt() {
-	char *input = NULL;
-	size_t size = 0;
+    char *input = NULL;
+    size_t size = 0;
 
-	while (true) {
-		printf(" > ");
-		size_t len = getline(&input, &size, stdin);
-		run(input);
+    while (true) {
+        printf(" > ");
+        size_t len = getline(&input, &size, stdin);
+        run(input);
 
-		if (len <= 1) {
-			break;
-		}
-	}
+        if (len <= 1) {
+            break;
+        }
+    }
 
-	free(input);
+    free(input);
 }
 
 int main(int argc, char **argv) {
-	if (argc == 2) {
-		runFile(argv[1]);
-	} else if (argc == 1)  {
-		runPrompt();
-	} else {
-		printf("Expects either 1 or 2 args.\n");
-	}
+    if (argc == 2) {
+        runFile(argv[1]);
+    } else if (argc == 1)  {
+        runPrompt();
+    } else {
+        printf("Expects either 1 or 2 args.\n");
+    }
 
-	return 0;
+    return 0;
 }
--- src/parser.c
+++ src/parser.c
@@ -9,350 +9,350 @@
 // parse parses the entire string into expressions, whilst parseExpr parses a single expression
 
 void freeExpr(Expr *expr) {
-	ListExpr *list = (ListExpr *)expr;
-	IdentifierExpr *identifier = (IdentifierExpr *)expr;
-	LiteralExpr *literal = (LiteralExpr *)expr;
+    ListExpr *list = (ListExpr *)expr;
+    IdentifierExpr *identifier = (IdentifierExpr *)expr;
+    LiteralExpr *literal = (LiteralExpr *)expr;
 
-	switch (expr->type) {
-		case LITERAL:
-		switch (literal->type) {
-			case STRING:
-			free(literal->string);
-			break;
-			default:
-			break;
-		}
-		break;
-		case LIST:
-		// Free all exprs
-		free(list->exprs);
-		break;
-		case IDENTIFIER:
-		free(identifier->name);
-		break;
-	}
+    switch (expr->type) {
+        case LITERAL:
+        switch (literal->type) {
+            case STRING:
+            free(literal->string);
+            break;
+            default:
+            break;
+        }
+        break;
+        case LIST:
+        // Free all exprs
+        free(list->exprs);
+        break;
+        case IDENTIFIER:
+        free(identifier->name);
+        break;
+    }
 }
 
 void consumeWhitespace(Parser *parser) {
-	while (parser->current < parser->sourceLength) {
-		char ch = parser->source[parser->current];
+    while (parser->current < parser->sourceLength) {
+        char ch = parser->source[parser->current];
 
-		// Ignore whitespace, besdies \n, in which case increment the line.
-		switch (ch) {
-			case ' ':
-			break;
-			case '\t':
-			break;
-			case '\r':
-			break;
-			case '\n':
-			parser->line++;
-			break;
-			default:
-			return;
-		}
+        // Ignore whitespace, besdies \n, in which case increment the line.
+        switch (ch) {
+            case ' ':
+            break;
+            case '\t':
+            break;
+            case '\r':
+            break;
+            case '\n':
+            parser->line++;
+            break;
+            default:
+            return;
+        }
 
-		parser->current++;
-	}
+        parser->current++;
+    }
 }
 
 ParserError parseListExpr(Parser *parser, ListExpr *expr) {
-	ParserError maybeErr;
-	parser->current++; // consume the left parenthesis
-	consumeWhitespace(parser);
+    ParserError maybeErr;
+    parser->current++; // consume the left parenthesis
+    consumeWhitespace(parser);
 
-	if (parser->source[parser->current] == ')') {
-		maybeErr.errorType = UNEXPECTED_CHAR;
-		maybeErr.line = parser->line;
-		maybeErr.ch = ')';
-		maybeErr.where = parser->current;
-		return maybeErr;
-	}
+    if (parser->source[parser->current] == ')') {
+        maybeErr.errorType = UNEXPECTED_CHAR;
+        maybeErr.line = parser->line;
+        maybeErr.ch = ')';
+        maybeErr.where = parser->current;
+        return maybeErr;
+    }
 
-	ListExpr listExpr;
-	listExpr.base = (Expr){ .type = LIST };
+    ListExpr listExpr;
+    listExpr.base = (Expr){ .type = LIST };
 
-	listExpr.exprsSize = 2;
-	listExpr.exprsCount = 0;
+    listExpr.exprsSize = 2;
+    listExpr.exprsCount = 0;
 
-	listExpr.exprs = calloc(listExpr.exprsSize, sizeof(Expr **));
+    listExpr.exprs = calloc(listExpr.exprsSize, sizeof(Expr **));
 
-	while (true) {
-		Expr *e;
-		ParserError err = parseExpr(parser, &e);
+    while (true) {
+        Expr *e;
+        ParserError err = parseExpr(parser, &e);
 
-		if (err.errorType != NONE) {
-			return err;
-		}
+        if (err.errorType != NONE) {
+            return err;
+        }
 
-		if (listExpr.exprsCount >= listExpr.exprsSize) {
-			listExpr.exprsSize += 2; // less aggressive growth
-			listExpr.exprs = realloc(listExpr.exprs, listExpr.exprsSize * sizeof(Expr *));
-		}
+        if (listExpr.exprsCount >= listExpr.exprsSize) {
+            listExpr.exprsSize += 2; // less aggressive growth
+            listExpr.exprs = realloc(listExpr.exprs, listExpr.exprsSize * sizeof(Expr *));
+        }
 
-		listExpr.exprs[listExpr.exprsCount++] = e;
+        listExpr.exprs[listExpr.exprsCount++] = e;
 
-		// look for a comma, if theres a comma, continue, if not, look for a ')'
-		char ch = parser->source[parser->current];
-		if (ch == ',') {
-			consumeWhitespace(parser);
-			continue;
-		}
+        // look for a comma, if theres a comma, continue, if not, look for a ')'
+        char ch = parser->source[parser->current];
+        if (ch == ',') {
+            consumeWhitespace(parser);
+            continue;
+        }
 
-		if (ch == ')') {
-			break;
-		}
+        if (ch == ')') {
+            break;
+        }
 
-		// error here
-		maybeErr.errorType = UNEXPECTED_CHAR;
-		maybeErr.line = parser->line;
-		maybeErr.ch = parser->source[parser->current];
-		maybeErr.where = parser->current;
-		return maybeErr;
-	}
+        // error here
+        maybeErr.errorType = UNEXPECTED_CHAR;
+        maybeErr.line = parser->line;
+        maybeErr.ch = parser->source[parser->current];
+        maybeErr.where = parser->current;
+        return maybeErr;
+    }
 
-	maybeErr.errorType = NONE;
-	return maybeErr;
+    maybeErr.errorType = NONE;
+    return maybeErr;
 }
 
 ParserError parseString(Parser *parser, LiteralExpr *expr) {
-	ParserError maybeError;
+    ParserError maybeError;
 
-	parser->current++; // Consume the opening "
+    parser->current++; // Consume the opening "
 
-	size_t strSize = 10;
-	size_t charCount = 0;
-	char *str = calloc(strSize, sizeof(char));
+    size_t strSize = 10;
+    size_t charCount = 0;
+    char *str = calloc(strSize, sizeof(char));
 
-	while (true) {
-		if (parser->current >= parser->sourceLength) {
-			free(str);
-			maybeError.errorType = MISSING_CHAR;
-			maybeError.line = parser->line;
-			maybeError.ch = '"';
-			maybeError.where = parser->current;
-			return maybeError;
-		}
+    while (true) {
+        if (parser->current >= parser->sourceLength) {
+            free(str);
+            maybeError.errorType = MISSING_CHAR;
+            maybeError.line = parser->line;
+            maybeError.ch = '"';
+            maybeError.where = parser->current;
+            return maybeError;
+        }
 
-		char ch = parser->source[parser->current];
-		if (ch == '"') break;
+        char ch = parser->source[parser->current];
+        if (ch == '"') break;
 
-		// Store character
-		if (charCount >= strSize) {
-			strSize *= 2;
-			str = realloc(str, strSize * sizeof(char));
-		}
+        // Store character
+        if (charCount >= strSize) {
+            strSize *= 2;
+            str = realloc(str, strSize * sizeof(char));
+        }
 
-		str[charCount++] = ch;
-		parser->current++;
-	}
+        str[charCount++] = ch;
+        parser->current++;
+    }
 
-	parser->current++; // Consume the closing "
+    parser->current++; // Consume the closing "
 
-	// Null-terminate the string
-	if (charCount >= strSize) {
-		str = realloc(str, (strSize + 1) * sizeof(char));
-	}
-	str[charCount] = '\0';
+    // Null-terminate the string
+    if (charCount >= strSize) {
+        str = realloc(str, (strSize + 1) * sizeof(char));
+    }
+    str[charCount] = '\0';
 
-	LiteralExpr stringLiteral;
-	stringLiteral.type = STRING;
-	stringLiteral.base = (Expr){ .type = LITERAL };
-	stringLiteral.string = str;
-	*expr = stringLiteral;
+    LiteralExpr stringLiteral;
+    stringLiteral.type = STRING;
+    stringLiteral.base = (Expr){ .type = LITERAL };
+    stringLiteral.string = str;
+    *expr = stringLiteral;
 
-	maybeError.errorType = NONE;
-	return maybeError;
+    maybeError.errorType = NONE;
+    return maybeError;
 }
 
 ParserError parseNumber(Parser *parser, LiteralExpr *expr) {
-	ParserError maybeError;
+    ParserError maybeError;
 
-	size_t strSize = 5;
-	size_t digitCount = 0;
-	char *number = calloc(strSize, sizeof(char));
-	bool foundDecimal = false;
+    size_t strSize = 5;
+    size_t digitCount = 0;
+    char *number = calloc(strSize, sizeof(char));
+    bool foundDecimal = false;
 
-	while (
-		parser->current < parser->sourceLength
-	) {
-		char ch = parser->source[parser->current++];
+    while (
+        parser->current < parser->sourceLength
+    ) {
+        char ch = parser->source[parser->current++];
 
-		if (!isdigit(ch)) {
-			if (ch == '.') {
-				if (foundDecimal) {
-					free(number);
-					maybeError.errorType = UNEXPECTED_CHAR;
-					maybeError.where = parser->current;
-					maybeError.line = parser->line;
-					maybeError.ch = '.';
-					return maybeError;
-				}
+        if (!isdigit(ch)) {
+            if (ch == '.') {
+                if (foundDecimal) {
+                    free(number);
+                    maybeError.errorType = UNEXPECTED_CHAR;
+                    maybeError.where = parser->current;
+                    maybeError.line = parser->line;
+                    maybeError.ch = '.';
+                    return maybeError;
+                }
 
-				foundDecimal = true;
-			} else {
-				break;
-			}
-		}
+                foundDecimal = true;
+            } else {
+                break;
+            }
+        }
 
-		if (digitCount >= strSize) {
-			strSize *= 2;
-			number = realloc(number, strSize * sizeof(char));
-		}
+        if (digitCount >= strSize) {
+            strSize *= 2;
+            number = realloc(number, strSize * sizeof(char));
+        }
 
-		number[digitCount++] = ch;
-	}
+        number[digitCount++] = ch;
+    }
 
-	// Make room for the null terminator.
-	if (digitCount >= strSize) {
-		strSize += 1;
-		number = realloc(number, strSize * sizeof(char));
-	}
+    // Make room for the null terminator.
+    if (digitCount >= strSize) {
+        strSize += 1;
+        number = realloc(number, strSize * sizeof(char));
+    }
 
-	number[digitCount] = '\0';
+    number[digitCount] = '\0';
 
-	LiteralExpr literal;
-	literal.base = (Expr){ .type = LITERAL };
-	literal.type = NUMBER;
-	literal.number = atof(number);
-	free(number);
-	*expr = literal;
+    LiteralExpr literal;
+    literal.base = (Expr){ .type = LITERAL };
+    literal.type = NUMBER;
+    literal.number = atof(number);
+    free(number);
+    *expr = literal;
 
-	maybeError.errorType = NONE;
-	return maybeError;
+    maybeError.errorType = NONE;
+    return maybeError;
 }
 
 ParserError parseIdentifier(Parser *parser, IdentifierExpr *expr) {
-	ParserError maybeError;
+    ParserError maybeError;
 
-	size_t strSize = 10;
-	size_t charCount = 0;
-	char *str = calloc(strSize, sizeof(char));
+    size_t strSize = 10;
+    size_t charCount = 0;
+    char *str = calloc(strSize, sizeof(char));
 
-	while (
-		parser->current < parser->sourceLength &&
-		isalpha(parser->source[parser->current])
-	) {
-		char ch = parser->source[parser->current++];
+    while (
+        parser->current < parser->sourceLength &&
+        isalpha(parser->source[parser->current])
+    ) {
+        char ch = parser->source[parser->current++];
 
-		if (charCount >= strSize) {
-			strSize *= 2;
-			str = realloc(str, strSize * sizeof(char));
-		}
+        if (charCount >= strSize) {
+            strSize *= 2;
+            str = realloc(str, strSize * sizeof(char));
+        }
 
-		str[charCount++] = ch;
-	}
+        str[charCount++] = ch;
+    }
 
-	// Make room for the null terminator.
-	if (charCount >= strSize) {
-		strSize += 1;
-		str = realloc(str, strSize * sizeof(char));
-	}
+    // Make room for the null terminator.
+    if (charCount >= strSize) {
+        strSize += 1;
+        str = realloc(str, strSize * sizeof(char));
+    }
 
-	str[charCount] = '\0';
+    str[charCount] = '\0';
 
-	IdentifierExpr identifier;
-	identifier.base = (Expr){ .type = IDENTIFIER };
-	identifier.name = str;
-	*expr = identifier;
+    IdentifierExpr identifier;
+    identifier.base = (Expr){ .type = IDENTIFIER };
+    identifier.name = str;
+    *expr = identifier;
 
-	maybeError.errorType = NONE;
-	return maybeError;
+    maybeError.errorType = NONE;
+    return maybeError;
 }
 
 ParserError parseExpr(Parser *parser, Expr **expr) {
-	ParserError err;
-	consumeWhitespace(parser);
+    ParserError err;
+    consumeWhitespace(parser);
 
-	if (parser->current >= parser->sourceLength) {
-		*expr = NULL;
-		err.errorType = NONE;
-		return err;
-	}
+    if (parser->current >= parser->sourceLength) {
+        *expr = NULL;
+        err.errorType = NONE;
+        return err;
+    }
 
-	char ch = parser->source[parser->current];
+    char ch = parser->source[parser->current];
 
-	union {
-		ListExpr list;
-		LiteralExpr literal;
-		IdentifierExpr identifier;
-	} exp;
+    union {
+        ListExpr list;
+        LiteralExpr literal;
+        IdentifierExpr identifier;
+    } exp;
 
-	ParserError maybeErr;
-	*expr = NULL;
-	switch (ch) {
-		case '(':
-		maybeErr = parseListExpr(parser, &exp.list);
-		*expr = (Expr *)&exp.list;
-		break;
-		case '"':
-		maybeErr = parseString(parser, &exp.literal);
-		*expr = (Expr *)&exp.literal;
-		break;
-		default:
-		if (isdigit(ch)) {
-			maybeErr = parseNumber(parser, &exp.literal);
-			*expr = (Expr *)&exp.literal;
-		} else if (isalpha(ch)) {
-			maybeErr = parseIdentifier(parser, &exp.identifier);
-			*expr = (Expr *)&exp.identifier;
-		} else {
-			maybeErr.errorType = UNEXPECTED_CHAR;
-			maybeErr.line = parser->line;
-			maybeErr.where = parser->current;
-			maybeErr.ch = ch;
-		}
+    ParserError maybeErr;
+    *expr = NULL;
+    switch (ch) {
+        case '(':
+        maybeErr = parseListExpr(parser, &exp.list);
+        *expr = (Expr *)&exp.list;
+        break;
+        case '"':
+        maybeErr = parseString(parser, &exp.literal);
+        *expr = (Expr *)&exp.literal;
+        break;
+        default:
+        if (isdigit(ch)) {
+            maybeErr = parseNumber(parser, &exp.literal);
+            *expr = (Expr *)&exp.literal;
+        } else if (isalpha(ch)) {
+            maybeErr = parseIdentifier(parser, &exp.identifier);
+            *expr = (Expr *)&exp.identifier;
+        } else {
+            maybeErr.errorType = UNEXPECTED_CHAR;
+            maybeErr.line = parser->line;
+            maybeErr.where = parser->current;
+            maybeErr.ch = ch;
+        }
 
-		break;
-	}
+        break;
+    }
 
-	if (maybeErr.errorType != NONE) {
-		return maybeErr;
-	}
+    if (maybeErr.errorType != NONE) {
+        return maybeErr;
+    }
 
-	err.errorType = NONE;
-	return err;
+    err.errorType = NONE;
+    return err;
 }
 
 void addExpr(Parser *parser, Expr *expr) {
-	if (parser->exprsCount >= parser->exprsSize) {
-		parser->exprsSize *= 2;
-		parser->exprs = realloc(parser->exprs, parser->exprsSize * sizeof(Expr *));
-	}
+    if (parser->exprsCount >= parser->exprsSize) {
+        parser->exprsSize *= 2;
+        parser->exprs = realloc(parser->exprs, parser->exprsSize * sizeof(Expr *));
+    }
 
-	parser->exprs[parser->exprsCount++] = expr;
+    parser->exprs[parser->exprsCount++] = expr;
 }
 
 ParserError parse(const char *source, size_t *tokensSize, Expr ***resultExprs) {
-	ParserError err;
-	size_t currentSize = 10;
-	Expr **exprs = calloc(currentSize, sizeof(Expr *));
+    ParserError err;
+    size_t currentSize = 10;
+    Expr **exprs = calloc(currentSize, sizeof(Expr *));
 
-	Parser parser = {
-		.source = source,
-		.sourceLength = strlen(source),
-		.current = 0,
-		.line = 1,
-		.exprs = exprs,
-		.exprsSize = currentSize,
-		.exprsCount = 0
-	};
+    Parser parser = {
+        .source = source,
+        .sourceLength = strlen(source),
+        .current = 0,
+        .line = 1,
+        .exprs = exprs,
+        .exprsSize = currentSize,
+        .exprsCount = 0
+    };
 
-	while (parser.current < parser.sourceLength) {
-		Expr *expr;
-		ParserError maybeErr = parseExpr(&parser, &expr);
+    while (parser.current < parser.sourceLength) {
+        Expr *expr;
+        ParserError maybeErr = parseExpr(&parser, &expr);
 
-		if (maybeErr.errorType != NONE) {
-			return maybeErr;
-		}
+        if (maybeErr.errorType != NONE) {
+            return maybeErr;
+        }
 
-		if (expr) {
-			addExpr(&parser, expr);
-		}
-	}
+        if (expr) {
+            addExpr(&parser, expr);
+        }
+    }
 
-	*resultExprs = parser.exprs;
-	*tokensSize = parser.exprsCount;
-	err.errorType = NONE;
-	return err;
+    *resultExprs = parser.exprs;
+    *tokensSize = parser.exprsCount;
+    err.errorType = NONE;
+    return err;
 }
