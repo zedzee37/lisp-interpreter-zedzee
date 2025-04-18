d69a69ebae1de0f558adc9a32a6b384b30e73956
zedzee <carsonuderitz@gmail.com>
2025-04-18 12:09:03 -0400

    objects

---
+++ include/object.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "expr.h"
+#include <stddef.h>
+#include <stdint.h>
+
+enum {
+    NUMBER_ID,
+    STRING_ID,
+    FUNCTION_ID,
+    CFUNCTION_ID,
+    OBJECT_ID
+};
+
+typedef struct {
+    uint32_t objectId;
+    void *value;
+} Object;
+
+typedef struct {
+    double num;
+} NumberObject;
+
+typedef struct {
+    char *str;
+} StringObject;
+
+typedef struct {
+    Expr **exprs;
+    size_t exprsCount;
+    size_t exprsSize;
+    size_t argCount;
+} FunctionObject;
+
+typedef Object *(*CFunc)(Object *, size_t);
+
+typedef struct {
+    size_t argCount;
+    CFunc fn;
+} CFunctionObject;
+
